e97ae84d38ade255c655322f6aab1ceba15924e2
PedroArias <emanuel.arias01@gmail.com>
2024-11-25 19:51:36 -0600

    Undo changes

--- backend/server.js
+++ backend/server.js
@@ -125,9 +125,3 @@ io.on("connection", (socket) => {
 
 })
 
-//deploy
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-})
