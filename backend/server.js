cc495cdfe25665143fca36a9febad3fb01819a9d
PedroArias <emanuel.arias01@gmail.com>
2024-11-25 21:05:11 -0600

    port changed

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,11 @@ app.post("/api/login/user/", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(5000, console.log(`"SERVER STARTED AT PORT ${PORT}"`.yellow.bold))
+//const server = app.listen(5000, console.log(`"SERVER STARTED AT PORT ${PORT}"`.yellow.bold))
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`.yellow.bold);
+});
 //CORS configuracion socket.io
 const io = require('socket.io')(server, {
     cors: {
